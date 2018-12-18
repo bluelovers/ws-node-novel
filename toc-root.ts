6cba4c17ecdbfbc43efcf4bec0ceb7e25ad9f025
bluelovers <codelovers@users.sourceforge.net>
2018-12-18 14:15:54 +0800

    .

--- toc-root.ts
+++ toc-root.ts
@@ -304,9 +304,9 @@ export function stringifyDataAuthor<T extends IMdconfMeta = IMdconfMeta>(data: I
 
 	let authors_anchor = authors.map(name => {
 		return `[${md_link_escape(name)}](#${md_anchor_gitee(name)})`;
-	}).join('  ／  ') + '\n';
+	}).join('  ／  ');
 
-	arr.push(authors_anchor);
+	arr.push(`> ${authors_anchor}'\n'`);
 
 	arr = arr.concat(arr_author);
 
