7ca1de89771574ee2150c9b30a44b0b64e35cdb4
bluelovers <codelovers@users.sourceforge.net>
2018-12-25 19:53:51 +0800

    1.1.23

--- test/demo.ts
+++ test/demo.ts
@@ -2,13 +2,16 @@
  * Created by user on 2018/2/9/009.
  */
 
-import novelText from '../';
+import novelText, { ITextLayoutOptions } from '../';
 import fs = require('fs-extra');
 
 let text = '';
-let options = {};
+let options: ITextLayoutOptions = {
+	allow_lf2: false,
+	allow_lf3: true,
+};
 
-text = fs.readFileSync('D:/Users/Documents/The Project/nodejs-test/node-novel2/dist_novel/user/黑之魔王/00270_第２７章：アイシテル/第５２８話　戦功交渉.txt').toString();
+text = fs.readFileSync('D:/Users/Documents/The Project/nodejs-test/node-novel2/dist_novel/user/黑之魔王/00270_第２７章：アイシテル/第５３０話　新たなる防具を求めて.txt').toString();
 
 let new_text = novelText.toStr(text);
 
