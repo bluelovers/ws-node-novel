eed2886fa2e38826aa6912b0ff86f205727fdca1
bluelovers <codelovers@users.sourceforge.net>
2019-05-30 08:58:15 +0800

    publish new version

--- packages/layout/index.ts
+++ packages/layout/index.ts
@@ -4,6 +4,7 @@
 
 export * from './lib';
 import { TextLayout, create } from './lib';
+export { IWordsAll, IWordsFunction, IWordsArray, IWordsArray2, IWordsUser, IWordsRuntime, IConstructorOptions, ITextLayoutOptions } from './lib/types';
 
 /**
  * 預設的 排版處理核心 如需要自訂預設值 可以 使用 `TextLayout.create(options)`
